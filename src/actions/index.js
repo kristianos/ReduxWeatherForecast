126ab05646420fab6d21fe94b193d53ec2b13f42
kristianos <albert_93_albert@yahoo.com>
2016-06-25 11:04:52 +0100

    Added weather API and axios package

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = '65ce39165683274f849c1b1a48ba7881';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
